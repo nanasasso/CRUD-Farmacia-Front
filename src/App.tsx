6bdbf26fe9df2773dcbb3a66ef6ab6978297d9c8
Fernanda Sasso <fsasso.23@gmail.com>
2024-02-28 18:45:46 -0300

    crud categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 //import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
 import Navbar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './paginas/home/Home';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategorias from './components/categorias/formularioCategorias/FormularioCategorias';
+import DeletarCategorias from './components/categorias/deletarCategorias/DeletarCategorias';
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategorias />} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategorias />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategorias />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
